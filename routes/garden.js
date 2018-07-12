d9874a41c3849655c73c5f6ed532a2b1658f4885
Taylor <tayjed2010@gmail.com>
2018-07-12 12:04:23 -0600

    adding backend

---
+++ routes/garden.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const knex = require('../db/knex');
+
+const queries = require('../queries');
+
+router.get("/", (request, response, next) => {
+    queries.list().then(gardens => {
+        response.json({gardens});
+    }).catch(next);
+});
+
+router.get("/:id", (request, response, next) => {
+    queries.read(request.params.id).then(garden => {
+        garden
+            ? response.json({garden})
+            : response.status(404).json({message: 'Not found'})
+    }).catch(next);
+});
+
+router.post("/", (request, response, next) => {
+    queries.create(request.body).then(garden => {
+        response.status(201).json({garden});
+    }).catch(next);
+});
+
+router.delete("/:id", (request, response, next) => {
+    queries.delete(request.params.id).then(() => {
+        response.status(204).json({deleted: true});
+    }).catch(next);
+});
+
+router.put("/:id", (request, response, next) => {
+    queries.update(request.params.id, request.body).then(garden => {
+        response.json({garden});
+    }).catch(next);
+});
+
+module.exports = router;
